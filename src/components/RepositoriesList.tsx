8399d3dbf9a1ba48c5701c783cc4a320b5dfdf7f
Yasu1025 <sillyjp25@gmail.com>
2022-01-12 23:54:12 +0900

    build search

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { actionsCreators } from '../state'
 
 export const RepositoriesList = () => {
   const [term, setTerm] = useState('')
+  const dispatch = useDispatch()
   const submit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    dispatch(actionsCreators.searchRepositories(term))
   }
   return (
     <div>
